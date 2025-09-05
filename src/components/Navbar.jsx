5b2d5155b9537982bd5975f0210128bb6ec18707
Lobo <loboateresa@gmail.com>
2025-09-04 23:14:46 -0500

    add hamburger menu to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="navbar">
-        <div className="navbar-left">
-            Alberto Castellano Mora, PhD
-        </div>
+    const [menuOpen, setMenuOpen] = useState(false);
 
-        <div className="navbar-right">
-            <Link to="/" className="nav-link">
-                Home
-            </Link>
-            <Link to="/about" className="nav-link">
-                About Me
-            </Link>
-            <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/research" className="nav-link">
-                Research
-            </Link>
-            <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/teaching" className="nav-link">
-                Teaching
-            </Link>
-            <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/scientific-outreach" className="nav-link">
-                Scientific Outreach
-            </Link>
-            <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/perks" className="nav-link">
-                Perks
-            </Link>
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                Alberto Castellano Mora, PhD
+            </div>
 
-        </div>
-    </nav>
-  );
+            <button 
+                className="menu-toggle"
+                aria-expanded={menuOpen}
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                ☰
+            </button>
+
+            <div className={`navbar-right ${menuOpen ? "open" : ""}`}>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/about" className="nav-link">About Me</Link>
+                <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/research" className="nav-link">
+                    Research
+                </Link>
+                <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/teaching" className="nav-link">
+                    Teaching
+                </Link>
+                <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/scientific-outreach" className="nav-link">
+                    Scientific Outreach
+                </Link>
+                <Link to="https://albertocastellanom.wixsite.com/alberto-castellano-m/perks" className="nav-link">
+                    Perks
+                </Link>
+            </div>
+
+            
+
+        </nav>
+    );
 }
 
 export default Navbar;
